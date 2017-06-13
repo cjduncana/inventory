f55c127c798aba3fda8dd7f90220cb775f40aeff
Christopher Duncan <cj.duncan.a@gmail.com>
2017-06-13 06:43:40 -0500

    :art: Refactor the Dialog Update file

--- src/Updates/Dialog.elm
+++ src/Updates/Dialog.elm
@@ -5,39 +5,7 @@ import Material
 import Maybe.Extra as Maybe
 import Model exposing (Model)
 import Models.Brand as Brand
-import Models.Dialog as Dialog
-    exposing
-        ( DialogView
-            ( AddBrand
-            , AddGood
-            , AddMarket
-            , EditGood
-            , EditView
-            )
-        , Msg
-            ( AddFileDialog
-            , BrandAdd
-            , BrandAddDialog
-            , BrandDropdownMsg
-            , EditDialog
-            , GoodAdd
-            , GoodAddDialog
-            , GoodBrandChange
-            , GoodEdit
-            , GoodEditDialog
-            , GoodMarketAdd
-            , GoodMarketRemove
-            , ImageSaved
-            , MarketAdd
-            , MarketAddDialog
-            , MarketDropdownMsg
-            , Mdl
-            , NameUpdate
-            , NewReportPage
-            , ObjectEdit
-            , RemoveImage
-            )
-        )
+import Models.Dialog as Dialog exposing (Msg)
 import Models.Dropdown as Dropdown
 import Models.Good as Good exposing (ImageURI(HasImage, NoImage))
 import Models.Market as Market
@@ -48,275 +16,210 @@ import Routing.Reports as Reports
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case ( msg, model.route ) of
-        ( Mdl msg_, _ ) ->
-            Material.update Mdl msg_ model
+        ( Dialog.Mdl msg_, _ ) ->
+            Material.update Dialog.Mdl msg_ model
 
-        ( NameUpdate name, _ ) ->
-            let
-                dialogView_ =
-                    Dialog.mapName (always name) model.dialogView
+        ( Dialog.NameUpdate name, _ ) ->
+            Cmd.none
+                |> (,)
+                    { model
+                        | dialogView =
+                            Dialog.mapName
+                                (always name)
+                                model.dialogView
+                    }
 
-                model_ =
-                    { model | dialogView = dialogView_ }
-            in
-                ( model_, Cmd.none )
+        ( Dialog.BrandAdd name, _ ) ->
+            Brand.createBrand
+                |> commandIfHasName name
+                |> (,) { model | dialogView = Dialog.AddBrand "" }
 
-        ( BrandAdd name, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = AddBrand "" }
+        ( Dialog.BrandAddDialog, _ ) ->
+            Cmd.none
+                |> (,) { model | dialogView = Dialog.AddBrand "" }
 
-                command_ =
-                    Brand.createBrand
-                        |> commandIfHasName name
-            in
-                ( model_, command_ )
+        ( Dialog.GoodAdd name data, _ ) ->
+            flip Good.createGood data
+                |> commandIfHasName name
+                |> (,) { model | dialogView = Dialog.newAddGoodView }
 
-        ( BrandAddDialog, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = AddBrand "" }
-            in
-                ( model_, Cmd.none )
+        ( Dialog.GoodAddDialog, _ ) ->
+            Cmd.none
+                |> (,) { model | dialogView = Dialog.newAddGoodView }
 
-        ( GoodAdd name data, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = Dialog.newAddGoodView }
+        ( Dialog.GoodEdit good, _ ) ->
+            always (Good.editGood good)
+                |> commandIfHasName (Good.getName good)
+                |> (,) { model | dialogView = Dialog.newAddGoodView }
 
-                command_ =
-                    flip Good.createGood data
-                        |> commandIfHasName name
-            in
-                ( model_, command_ )
+        ( Dialog.GoodEditDialog good, _ ) ->
+            Cmd.none
+                |> (,) { model | dialogView = Dialog.newEditGoodView good }
 
-        ( GoodAddDialog, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = Dialog.newAddGoodView }
-            in
-                ( model_, Cmd.none )
+        ( Dialog.GoodBrandChange maybeBrand, _ ) ->
+            Cmd.none
+                |> (,)
+                    { model
+                        | dialogView =
+                            Dialog.setBrand
+                                maybeBrand
+                                model.dialogView
+                    }
 
-        ( GoodEdit good, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = Dialog.newAddGoodView }
+        ( Dialog.GoodMarketAdd maybeMarket, _ ) ->
+            Cmd.none
+                |> (,)
+                    { model
+                        | dialogView =
+                            Dialog.addMarket
+                                maybeMarket
+                                model.dialogView
+                    }
 
-                command_ =
-                    always (Good.editGood good)
-                        |> commandIfHasName (Good.getName good)
-            in
-                ( model_, command_ )
+        ( Dialog.GoodMarketRemove index, _ ) ->
+            Cmd.none
+                |> (,)
+                    { model
+                        | dialogView =
+                            Dialog.removeMarket
+                                index
+                                model.dialogView
+                    }
 
-        ( GoodEditDialog good, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = Dialog.newEditGoodView good }
-            in
-                ( model_, Cmd.none )
+        ( Dialog.MarketAdd name, _ ) ->
+            Market.createMarket
+                |> commandIfHasName name
+                |> (,) { model | dialogView = Dialog.AddMarket "" }
 
-        ( GoodBrandChange maybeBrand, _ ) ->
-            let
-                dialogView_ =
-                    Dialog.setBrand maybeBrand model.dialogView
+        ( Dialog.MarketAddDialog, _ ) ->
+            Cmd.none
+                |> (,) { model | dialogView = Dialog.AddMarket "" }
 
-                model_ =
-                    { model | dialogView = dialogView_ }
-            in
-                ( model_, Cmd.none )
+        ( Dialog.EditDialog object, _ ) ->
+            Cmd.none
+                |> (,)
+                    { model
+                        | dialogView = Dialog.EditView object object.name
+                    }
 
-        ( GoodMarketAdd maybeMarket, _ ) ->
-            let
-                dialogView_ =
-                    Dialog.addMarket maybeMarket model.dialogView
+        ( Dialog.ObjectEdit object, Routes.Brands ) ->
+            always (Brand.editBrand object)
+                |> commandIfHasName object.name
+                |> (,) { model | dialogView = Dialog.AddBrand "" }
 
-                model_ =
-                    { model | dialogView = dialogView_ }
-            in
-                ( model_, Cmd.none )
+        ( Dialog.ObjectEdit object, Routes.Markets ) ->
+            always (Market.editMarket object)
+                |> commandIfHasName object.name
+                |> (,) { model | dialogView = Dialog.AddMarket "" }
 
-        ( GoodMarketRemove index, _ ) ->
-            let
-                dialogView_ =
-                    Dialog.removeMarket index model.dialogView
-
-                model_ =
-                    { model | dialogView = dialogView_ }
-            in
-                ( model_, Cmd.none )
-
-        ( MarketAdd name, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = AddMarket "" }
-
-                command_ =
-                    Market.createMarket
-                        |> commandIfHasName name
-            in
-                ( model_, command_ )
-
-        ( MarketAddDialog, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = AddMarket "" }
-            in
-                ( model_, Cmd.none )
-
-        ( EditDialog object, _ ) ->
-            let
-                model_ =
-                    { model | dialogView = EditView object object.name }
-            in
-                ( model_, Cmd.none )
-
-        ( ObjectEdit object, Routes.Brands ) ->
-            let
-                model_ =
-                    { model | dialogView = AddBrand "" }
-
-                command_ =
-                    always (Brand.editBrand object)
-                        |> commandIfHasName object.name
-            in
-                ( model_, command_ )
-
-        ( ObjectEdit object, Routes.Markets ) ->
-            let
-                model_ =
-                    { model | dialogView = AddMarket "" }
-
-                command_ =
-                    always (Market.editMarket object)
-                        |> commandIfHasName object.name
-            in
-                ( model_, command_ )
-
-        ( ObjectEdit _, _ ) ->
+        ( Dialog.ObjectEdit _, _ ) ->
             ( model, Cmd.none )
 
-        ( AddFileDialog, _ ) ->
+        ( Dialog.AddFileDialog, _ ) ->
             ( model, Dialog.addFileDialog )
 
-        ( ImageSaved filename, _ ) ->
+        ( Dialog.ImageSaved filename, _ ) ->
             changeImage model <| Just filename
 
-        ( RemoveImage, _ ) ->
+        ( Dialog.RemoveImage, _ ) ->
             changeImage model Nothing
 
-        ( BrandDropdownMsg msg_, _ ) ->
+        ( Dialog.BrandDropdownMsg msg_, _ ) ->
             case model.dialogView of
-                AddGood goodContent ->
-                    let
-                        ( brandDropdown_, command_ ) =
-                            Dropdown.update Dropdown.brandConfig
-                                msg_
-                                goodContent.brandDropdown
-
-                        dialogView_ =
-                            AddGood
-                                { goodContent
-                                    | brandDropdown = brandDropdown_
+                Dialog.AddGood goodContent ->
+                    Dropdown.update Dropdown.brandConfig
+                        msg_
+                        goodContent.brandDropdown
+                        |> Tuple.mapFirst
+                            (\newDropdown ->
+                                { model
+                                    | dialogView =
+                                        Dialog.AddGood
+                                            { goodContent
+                                                | brandDropdown = newDropdown
+                                            }
                                 }
+                            )
 
-                        model_ =
-                            { model | dialogView = dialogView_ }
-                    in
-                        ( model_, command_ )
-
-                EditGood good goodContent ->
-                    let
-                        ( brandDropdown_, command_ ) =
-                            Dropdown.update Dropdown.brandConfig
-                                msg_
-                                goodContent.brandDropdown
-
-                        dialogView_ =
-                            EditGood good
-                                { goodContent
-                                    | brandDropdown = brandDropdown_
+                Dialog.EditGood good goodContent ->
+                    Dropdown.update Dropdown.brandConfig
+                        msg_
+                        goodContent.brandDropdown
+                        |> Tuple.mapFirst
+                            (\newDropdown ->
+                                { model
+                                    | dialogView =
+                                        Dialog.EditGood good
+                                            { goodContent
+                                                | brandDropdown = newDropdown
+                                            }
                                 }
-
-                        model_ =
-                            { model | dialogView = dialogView_ }
-                    in
-                        ( model_, command_ )
+                            )
 
                 _ ->
                     ( model, Cmd.none )
 
-        ( MarketDropdownMsg msg_, _ ) ->
+        ( Dialog.MarketDropdownMsg msg_, _ ) ->
             case model.dialogView of
-                AddGood goodContent ->
-                    let
-                        ( marketDropdown_, command_ ) =
-                            Dropdown.update Dropdown.marketConfig
-                                msg_
-                                goodContent.marketDropdown
-
-                        dialogView_ =
-                            AddGood
-                                { goodContent
-                                    | marketDropdown = marketDropdown_
+                Dialog.AddGood goodContent ->
+                    Dropdown.update Dropdown.marketConfig
+                        msg_
+                        goodContent.marketDropdown
+                        |> Tuple.mapFirst
+                            (\newDropdown ->
+                                { model
+                                    | dialogView =
+                                        Dialog.AddGood
+                                            { goodContent
+                                                | marketDropdown = newDropdown
+                                            }
                                 }
+                            )
 
-                        model_ =
-                            { model | dialogView = dialogView_ }
-                    in
-                        ( model_, command_ )
-
-                EditGood good goodContent ->
-                    let
-                        ( marketDropdown_, command_ ) =
-                            Dropdown.update Dropdown.marketConfig
-                                msg_
-                                goodContent.marketDropdown
-
-                        dialogView_ =
-                            EditGood good
-                                { goodContent
-                                    | marketDropdown = marketDropdown_
+                Dialog.EditGood good goodContent ->
+                    Dropdown.update Dropdown.marketConfig
+                        msg_
+                        goodContent.marketDropdown
+                        |> Tuple.mapFirst
+                            (\newDropdown ->
+                                { model
+                                    | dialogView =
+                                        Dialog.EditGood good
+                                            { goodContent
+                                                | marketDropdown = newDropdown
+                                            }
                                 }
-
-                        model_ =
-                            { model | dialogView = dialogView_ }
-                    in
-                        ( model_, command_ )
+                            )
 
                 _ ->
                     ( model, Cmd.none )
 
-        ( NewReportPage, Routes.Reports ) ->
+        ( Dialog.NewReportPage, Routes.Reports ) ->
             Reports.gotoNew model
 
-        ( NewReportPage, _ ) ->
+        ( Dialog.NewReportPage, _ ) ->
             ( model, Cmd.none )
 
 
 commandIfHasName : String -> (String -> Cmd msg) -> Cmd msg
 commandIfHasName name function =
     Just name
-        |> Maybe.filter String.isEmpty
+        |> Maybe.filter (not << String.isEmpty)
         |> Maybe.map function
         |> Maybe.withDefault Cmd.none
 
 
 changeImage : Model -> Maybe String -> ( Model, Cmd Msg )
 changeImage model filename =
-    let
-        uri_ =
-            Maybe.map HasImage filename
-                |> Maybe.withDefault NoImage
-
-        dialogView =
-            Dialog.setImage uri_ model.dialogView
-
-        model_ =
-            { model | dialogView = dialogView }
-
-        command_ =
-            Maybe.next filename (Dialog.getFilename model.dialogView)
-                |> Maybe.map Dialog.removeImage
-                |> Maybe.withDefault Cmd.none
-    in
-        ( model_, command_ )
+    Maybe.next filename (Dialog.getFilename model.dialogView)
+        |> Maybe.map Dialog.removeImage
+        |> Maybe.withDefault Cmd.none
+        |> (,) filename
+        |> Tuple.mapFirst
+            (Maybe.map HasImage
+                >> Maybe.withDefault NoImage
+                >> \uri ->
+                    { model
+                        | dialogView = Dialog.setImage uri model.dialogView
+                    }
+            )
