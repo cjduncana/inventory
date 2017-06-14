0a4d6acabd9e0feb63890b1107875de4624316bf
Christopher Duncan <cj.duncan.a@gmail.com>
2017-06-13 20:16:22 -0500

    :art: Refactor commandIfEmpty in Utilities

--- src/Models/Utilities.elm
+++ src/Models/Utilities.elm
@@ -1,9 +1,11 @@
 module Models.Utilities exposing (commandIfEmpty)
 
+import Maybe.Extra as Maybe
+
 
 commandIfEmpty : (() -> Cmd msg) -> List a -> Cmd msg
-commandIfEmpty fetch list =
-    if List.isEmpty list then
-        fetch ()
-    else
-        Cmd.none
+commandIfEmpty fetch =
+    Just
+        >> Maybe.filter List.isEmpty
+        >> Maybe.map (always (fetch ()))
+        >> Maybe.withDefault Cmd.none
